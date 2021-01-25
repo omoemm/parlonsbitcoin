c9e7abcb2f99b9644135ba8a1e941b51ea5879fd
omoemm <omoemm@pm.me>
2021-01-25 08:45:11 -0500

    Add the tailwind playground as an inspiration

--- components/Nav.js
+++ components/Nav.js
@@ -4,20 +4,20 @@ export default function Nav() {
   return (
     <nav className="flex justify-between max-w-4xl w-full p-8 my-0 md:my-8 mx-auto">
       <NextLink href="/">
-        <a className="text-orange text-xl">Parlons Bitcoin</a>
+        <a className="text-orange text-2xl font-extrabold">Parlons Bitcoin</a>
       </NextLink>
       <div>
         <NextLink href="/episodes">
-          <a className="mr-6 font-medium text-lg">
+          <a className="mr-6 text-lg font-semibold">
             Épisodes
             </a>
         </NextLink>
         <NextLink href="/merci">
-          <a className="mr-6 font-medium text-lg">
+          <a className="mr-6 text-lg font-semibold">
             👑</a>
         </NextLink>
         <NextLink href="/about">
-          <a className="mr-6 font-medium text-lg">
+          <a className="mr-6 text-lg font-semibold">
             À Propos</a>
         </NextLink>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const colors = require('tailwindcss/colors')
+
+
 module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
@@ -6,7 +9,9 @@ module.exports = {
       colors:{
         orange: 'rgb(245, 146, 79)',
         white: 'rgb(252, 250, 249)',
-      }
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+      },
     },
   },
   variants: {
