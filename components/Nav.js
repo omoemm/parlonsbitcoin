fafc9a9d349e7af95d4495d575b19139d35c6dcb
omoemm <omoemm@pm.me>
2021-01-24 21:10:59 -0500

    Rework css Nav

--- components/Nav.js
+++ components/Nav.js
@@ -2,25 +2,25 @@ import NextLink from 'next/link'
 
 export default function Nav() {
   return (
-    <div className="flex justify-between max-w-4xl w-full p-8 mx-auto">
+    <nav className="flex justify-between max-w-4xl w-full p-8 my-0 md:my-8 mx-auto">
       <NextLink href="/">
-        <a className="text-orange">Parlons Bitcoin</a>
+        <a className="text-orange text-xl">Parlons Bitcoin</a>
       </NextLink>
       <div>
         <NextLink href="/episodes">
-          <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100">
+          <a className="mr-6 font-medium text-lg">
             Épisodes
             </a>
         </NextLink>
         <NextLink href="/merci">
-        <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100">
+          <a className="mr-6 font-medium text-lg">
             👑</a>
         </NextLink>
         <NextLink href="/about">
-        <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100">
+          <a className="mr-6 font-medium text-lg">
             À Propos</a>
         </NextLink>
       </div>
-    </div>
+    </nav>
   )
 }
