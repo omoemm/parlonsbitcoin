0de1a23df0bcd8dcffb74bed6053861f9d0804ca
omoemm <omoemm@pm.me>
2021-01-27 08:11:50 -0500

    Add quote of the day

--- components/Quote.js
+++ components/Quote.js
@@ -1,3 +1,8 @@
+quotes = [
+  "To be everywhere is to be nowhere.",
+  "It is not the man who has too little who is poor, but the one who hankers after more.",
+]
+
 export default function Quote() {
   const author = "Seneca"
   const quote = "To be everywhere is to be nowhere."
