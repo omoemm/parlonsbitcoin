247872335a37a5358990186d737cd27d2737dc51
omoemm <omoemm@pm.me>
2021-02-02 10:02:19 -0500

    WIP Paint description while hovering a podcast card

--- layouts/podcasts.js
+++ layouts/podcasts.js
@@ -1,14 +1,21 @@
+import React from 'react'
 import NextLink from 'next/link'
 
 function Card({ podcast }) {
+  const [isDescriptionShown, setIsDescriptionShown] = React.useState(false)
+
   return (
-    <div className="w-full shadow-lg  cursor-pointer bg-white rounded-lg sm:rounded-2xl p-1 sm:p-2.5">
+    <div
+    onMouseEnter={() => setIsDescriptionShown(true)}
+    onMouseLeave={() => setIsDescriptionShown(false)}
+    className="w-full shadow-lg  cursor-pointer  bg-white rounded-lg sm:rounded-2xl p-1 sm:p-2.5 hover:shadow-md">
       <NextLink href={`/podcasts/${podcast.slug}`}>
-        <div className="flex flex-col items-center">
-          <div class="relative ">
-            <img className="rounded-md" alt='podcast-tile' src={podcast.image} />
-            <p className="text-gray-600 mt-1 text-lg">{podcast.title}</p>
-          </div>
+        <div className="flex flex-col  items-center">
+          <img className="rounded-md" alt='podcast-tile' src={podcast.image} />
+          {isDescriptionShown &&
+            <div className="absolute italic p-5 inline-block my-32 max-w-xs text-center bg-gray-100 bg-opacity-95 rounded-md">{podcast.description}</div>
+          }
+          <p className="text-gray-600 mt-1 text-lg font-semibold p-2 text-center my-auto">{podcast.title}</p>
         </div>
       </NextLink>
     </div>
