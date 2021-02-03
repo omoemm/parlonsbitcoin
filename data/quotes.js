2e1bfaac117e4f02dcf1485f0985dc741ba49b16
omoemm <omoemm@pm.me>
2021-02-03 09:28:29 -0500

    Improve quote text

--- data/quotes.js
+++ data/quotes.js
@@ -1,13 +1,13 @@
 const quotesByAuthor = {
   Sénèque: [
     "Être partout à la fois, c'est être nulle part.",
-    "Ce n'est pas l'homme avec peu qui est pauvre, mais celui qui veut davantage.",
+    "Ce n'est pas l'homme qui possède peu qui est pauvre, mais celui qui désire davantage.",
     "Considère pendant un long moment l'idée  d'admettre une personne dans ton amitié. Mais lorsque tu as décidé de le faire, accueille la avec coeur et âme, et parle avec elle sans réserve comme tu le ferais avec toi-même.",
-    "Cela me rappèle cette chose que j'ai entendue à Pomponius. ‘Certains hommes se sont enfermés dans des coins si sombres que les objets à la lumière du jour leur paraissent flous.’",
+    "Cela me rappelle cette phrase entendue à Pomponius. ‘Certains hommes se sont enfermés dans des coins si sombres que les objets à la lumière du jour leur paraissent flous.’",
     "Les gens devraient admirer notre manière de vivre mais ils devraient en même temps la trouver compréhensible.",
     "Je vois avec plaisir et approuve la manière que tu as de rester à tes études et de sacrifier tout à tes efforts résolus pour faire de toi un homme meilleur chaque jour.",
     "Quiconque pénètre notre foyer, devrait nous admirer nous plutôt que nos meubles.",
-    "Une chose que j'ai remarquée avec l'auteur stoïque Hecato. Limiter ses désirs aide véritablement à guérir la peur. ‘Cesse d'espérer’, dit-il, ‘et tu cesseras d'avoir peur.’",
+    "Une chose que j'ai remarquée avec l'auteur stoïque Hecato. Limiter ses désirs aide à guérir la peur. ‘Cesse d'espérer’, dit-il, ‘et tu cesseras d'avoir peur.’",
     "La peur marche à l'unisson avec l'espoir. Leur relation ne me surprend pas, les deux appartiennent à un esprit en suspens, à un esprit anxieux de son regard vers le futur.",
     "Si la sagesse m'était offerte à la seule condition que je doive la garder pour moi seul et ne la divulguer à quiconque, je la rejetterais.",
     "Il n'y a aucun plaisir à posséder quelque chose de valeur sans avoir quelqu'un avec qui la partager.",
@@ -15,7 +15,7 @@ const quotesByAuthor = {
     "Tout aussi bonne est la réponse de l'artiste à qui l'on demande pourquoi se donner tout ce trouble: ‘Quelques personnes pour apprécier mon art sont suffisantes pour moi; une personne l'est également; aucune personne, c'est également suffisant.’",
     "La fin ressemble inévitablement au début: une personne qui démarre une amitié parce que ça lui profite cessera d'être amie lorsque cela ne lui profite plus.",
     "Offir son amitié seulement pour le meilleur mais pas pour le pire, c'est la dépouiller de toute dignité.",
-    "L'homme sage a besoin de ses mains, de ses yeux et d'un grand nombre de choses requises pour la vie de tous les jours; mais il ne manque de rien, car manquer de quelque chose signifie que c'est une nécessite et rien, à l'homme sage, n'est une nécessité.",
+    "L'homme sage (...) ne manque de rien, car manquer de quelque chose signifie que cette chose une nécessite et rien, à l'homme sage, n'est une nécessité.",
     "Non heureux est celui qui pense cela de lui-même.",
   ],
   "Lao Tzu": [
