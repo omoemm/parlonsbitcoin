872c0c8777aa178f86d93ab731eab07c499949a9
omoemm <omoemm@pm.me>
2021-02-19 15:15:23 -0500

    Add forms with arithmetic verification

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -1,17 +1,89 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import axios from 'axios'
+
+
+function ArithmeticValidator() {
+  return <div>hello</div>
+}
 
 
 export default function Newsletter() {
-  const [showForm, setShowFrom] = React.useState(false)
+  const inputEl = useRef(null)
+  const mathEl = useRef(null)
+  const [email, setEmail] = React.useState(null)
+  // const [form, setForm] = React.useState({ state: 'waitingEmail' })
+  const [form, setForm] = React.useState({ state: 'emailStored' })
+  const [number1, setNumber1] = React.useState(Math.floor(Math.random() * 11))
+  const [number2, setNumber2] = React.useState(Math.floor(Math.random() * 11))
+
+
+  const storeEmail = async (e) => {
+    e.preventDefault()
+    setEmail(inputEl.current.value)
+    setForm({ state: 'emailStored' })
+  }
+
+  const checkArithmetic = async (e) => {
+    e.preventDefault()
+  }
+
+  const postEmail = async () => {
+    try {
+      const res = await axios({
+        method: "POST",
+        url: "https://api.onearth.be/v1/parlonsbitcoin/mailing",
+        data: {
+          email: email,
+          token: process.env.TOKEN,
+        }
+      })
+      // check status == 201 - succesful POST
+    }
+    catch (error) {
+      // not successful POST -> try again
+      // console.log(error.response)
+    }
+  }
+
 
   return (
-      <div className="flex flex-col justify-center items-start max-w-md mx-auto my-8">
-        <button className="rounded-md shadow-md hover:shadow-sm font-bold text-lg md:text-xl tracking-tight border px-6 py-4 self-center w-full" onClick={() => setShowFrom(!showForm)}>
-          {showForm ? "➖" : "Ne manque aucun épisode, abonne-toi 🚀"}
-        </button>
-        {showForm &&
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfCHsiwmrZS1xcZRVD5D_Ok2HJ1XJzvqYItYgP636nKROBsNg/viewform?embedded=true" frameborder="0" marginheight="0" height="460" marginwidth="0" className="w-full">Loading…</iframe>
-        }
+    <div className="flex flex-col justify-center items-center max-w-md mx-auto my-8">
+      <div className="font-bold text-lg md:text-xl tracking-tight px-6 py-4 self-center w-full">Ne manque aucun épisode, abonne-toi 🚀
       </div>
+      {form.state === "waitingEmail" && (
+        <form className="relative my-2 shadow-md w-full" onSubmit={storeEmail}>
+          <input
+            ref={inputEl}
+            placeholder="satoshi@nakamoto.com"
+            type="email"
+            autoComplete="email"
+            required
+            className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+          />
+          <button
+            className="flex items-center justify-center absolute right-1 top-2 px-4 font-bold h-8 bg-gray-100 text-gray-900 rounded w-28"
+            type="submit"
+          >
+            S'abonner
+        </button>
+        </form>
+      )}
+      {form.state === "emailStored" && (
+        <form className="flex flex-row justify-center my-2 w-full space-x-4" onSubmit={checkArithmetic}>
+          <input
+            ref={mathEl}
+            placeholder={`Que vaut ${number1} + ${number2} ? `}
+            type="number"
+            autoComplete="number"
+            required
+            className="shadow-sm px-4 py-2 rounded-md bg-white text-gray-900"
+          />
+          <button
+          className="bg-white shadow-md px-2 py-2 font-bold border text-gray-900 rounded"
+          >Soumettre 🧮</button>
+        </form>
+      )}
+
+    </div>
   );
 }
