62cd5f932796a9a18a6d3cc14ebf76ae32e205bd
omoemm <omoemm@pm.me>
2021-02-20 09:05:06 -0500

    Add wip management for ep 2 and 3

--- layouts/podcasts.js
+++ layouts/podcasts.js
@@ -4,9 +4,10 @@ import NextLink from 'next/link'
 function Card({ podcast }) {
   return (
     <div
-      className="w-full shadow-lg  cursor-pointer  bg-white rounded-lg sm:rounded-2xl p-1 sm:p-2.5 hover:shadow-md">
-      <NextLink href={`/podcasts/${podcast.slug}`}>
-        <div className="flex flex-col items-center relative h-full">
+      className="w-full shadow-lg  bg-white rounded-lg sm:rounded-2xl p-1 sm:p-2.5 hover:shadow-md">
+      {podcast.wip === true
+        ?
+        (<div className="flex flex-col items-center relative h-full cursor-not-allowed">
           <img className="rounded-md" alt='podcast-tile' src={podcast.image} />
           <div
             className="flex text-gray-600 text-sm md:text-lg font-medium p-2 text-center h-full items-center">
@@ -16,8 +17,24 @@ function Card({ podcast }) {
             className="absolute opacity-0 hover:opacity-95 transition-opacity md:p-5 top-0 bottom-0 right-0 left-0 flex items-center text-center md:text-lg bg-gray-50 bg-opacity-95 rounded-md text-gray-800">
             {podcast.description}
           </div>
-        </div>
-      </NextLink>
+        </div>)
+        : (
+
+          <NextLink href={`/podcasts/${podcast.slug}`}>
+            <div className="flex flex-col items-center relative h-full cursor-pointer">
+              <img className="rounded-md" alt='podcast-tile' src={podcast.image} />
+              <div
+                className="flex text-gray-600 text-sm md:text-lg font-medium p-2 text-center h-full items-center">
+                {podcast.title}
+              </div>
+              <div
+                className="absolute opacity-0 hover:opacity-95 transition-opacity md:p-5 top-0 bottom-0 right-0 left-0 flex items-center text-center md:text-lg bg-gray-50 bg-opacity-95 rounded-md text-gray-800">
+                {podcast.description}
+              </div>
+            </div>
+          </NextLink>
+        )
+      }
     </div>
   )
 }
