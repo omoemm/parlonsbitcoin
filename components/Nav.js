e6463b54e83dd0019b9d8a0f739c8a517b5d8af4
omoemm <omoemm@pm.me>
2021-03-05 08:52:04 -0500

    Rework mobile menu

--- components/Nav.js
+++ components/Nav.js
@@ -2,28 +2,28 @@ import NextLink from 'next/link'
 
 export default function Nav() {
   return (
-    <nav className="flex justify-between items-center max-w-4xl w-full p-6 md:p-8 my-0 md:my-8 mx-auto">
+    <nav className="flex flex-col space-y-2 md:flex-row justify-between items-center max-w-4xl w-full p-6 md:p-8 my-0 md:my-8 mx-auto">
       <NextLink href="/">
         <a className="text-orange text-2xl font-extrabold">Parlons Bitcoin</a>
       </NextLink>
-      <div className="flex flex-wrap justify-between md:flex md:flex-row md:items-center">
+      <div className="flex flex-row space-x-4 md:space-x-6 md:items-center">
         <NextLink href="/about">
-          <a className="mr-6 text-lg font-bold">
+          <a className="text-lg font-bold">
             À Propos
           </a>
         </NextLink>
         <NextLink href="/podcasts">
-          <a className="mr-6 text-lg font-bold">
+          <a className="text-lg font-bold">
             Podcasts
           </a>
         </NextLink>
         <NextLink href="/merci">
-          <a className="mr-6 text-lg font-bold">
+          <a className="text-lg font-bold">
             Merci
           </a>
         </NextLink>
         <NextLink href="/contact">
-          <a className="mr-6 text-lg font-bold">
+          <a className="text-lg font-bold">
             Contact
           </a>
         </NextLink>
